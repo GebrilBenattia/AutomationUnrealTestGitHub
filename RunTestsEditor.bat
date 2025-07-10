430e4a47c3980e2f224f6bde708856333fb4ac7d
Lukas TAMAYO <42295409+BinaryGleam@users.noreply.github.com>
2025-07-10 15:21:26 +0200

    Fixed those

--- RunTestsEditor.bat
+++ RunTestsEditor.bat
@@ -2,4 +2,4 @@ set log_path=%~dp0Saved\Logs\Tests\EditorTest.log
 REM echo %log_path%
 REM pause
 
-C:\Epic\UE_5.4\Engine\Binaries\Win64\UnrealEditor-Cmd.exe %~dp0GG.uproject -execcmds="Automation RunTests Project;Quit" -stdout -unattended -NOSPLASH -NullRHI -log -ABSLOG="%log_path%"
+C:\Epic\UE_5.4\Engine\Binaries\Win64\UnrealEditor-Cmd.exe %~dp0DemoAutomation.uproject -execcmds="Automation RunTests Project;Quit" -stdout -unattended -NOSPLASH -NullRHI -log -ABSLOG="%log_path%"
